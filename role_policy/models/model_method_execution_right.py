5b30b78fb14a7238c6d2255707c0765f2c721ab7
Sabas <s@electroniccats.com>
2026-01-25 19:38:23 -0600

    REF: Make ModelMethodExecutionRight lambda dynamic

--- role_policy/models/model_method_execution_right.py
+++ role_policy/models/model_method_execution_right.py
@@ -65,7 +65,7 @@ class ModelMethodExecutionRight(models.Model):
             return True
         user_roles = self.env.user.enabled_role_ids or self.env.user.role_ids
         model_methods = user_roles.mapped("model_method_ids").filtered(
-            lambda r: r.name == "account.move,post"
+            lambda r, n=name: r.name == n
             and r.company_id == self.env.user.company_id
             and r.active
         )
