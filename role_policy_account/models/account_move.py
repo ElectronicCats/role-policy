d9e806fd30b9e1fa53283ba27d419eb6ca2e5f8e
Sabas <s@electroniccats.com>
2026-01-25 19:19:36 -0600

    Updates account_move to use action_post
    The post() method in account.move was renamed to action_post() in Odoo 15.
    
    Updates the code to use the new method name for compatibility
    with newer Odoo versions.
    Also updates the copyright year.

--- role_policy_account/models/account_move.py
+++ role_policy_account/models/account_move.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Noviat
+# Copyright 2020-2024 Noviat
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models
@@ -7,10 +7,14 @@ from odoo import models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def post(self):
+    def action_post(self):
+        """Override action_post to check role policy execution rights.
+
+        Note: In Odoo 15+, the method was renamed from post() to action_post().
+        """
         self.env["model.method.execution.right"].check_right(
-            "account.move,post", raise_exception=True
+            "account.move,action_post", raise_exception=True
         )
         ctx = dict(self.env.context, role_policy_has_groups_ok=True)
         self = self.with_context(ctx)
-        return super().post()
+        return super().action_post()
--- role_policy_account/models/model_method_execution_right.py
+++ role_policy_account/models/model_method_execution_right.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Noviat
+# Copyright 2020-2024 Noviat
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models
@@ -9,7 +9,8 @@ class ModelMethodExecutionRight(models.Model):
 
     def _selection_name(self):
         model_methods = super()._selection_name()
-        account_model_methods = [("account.move,post")]
+        # Note: In Odoo 15+, post() was renamed to action_post()
+        account_model_methods = [("account.move,action_post")]
         model_methods += [(x, x) for x in account_model_methods]
         model_methods.sort()
         return model_methods
